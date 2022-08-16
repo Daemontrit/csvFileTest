5f0296877042de9633bd1f1777509c9f4c8aa779
Daemontrit <slavikk1999@yandex.ru>
2022-08-16 15:48:23 +0300

    test value range

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,12 @@
+package ru.netology.sqr;
+public class SQRService {
+    public int sqrtCount(int minRange, int maxRange){
+        int count=0;
+        for (int i = 0; i*i <= maxRange; i++) {
+            if (minRange<=i*i && i*i<=maxRange){
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,15 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class SQRServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/valuesRange.csv")
+    public void testFieldSqrt(int expected, int minRange, int maxRange){
+        SQRService service=new SQRService();
+        int actual=service.sqrtCount(minRange,maxRange);
+        Assertions.assertEquals(expected,actual);
+    }
+}
