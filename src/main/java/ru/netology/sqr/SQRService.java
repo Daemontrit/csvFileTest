1261295711450acbb50c58f879e459f1bd01f1f6
Daemontrit <slavikk1999@yandex.ru>
2022-08-16 15:55:19 +0300

    code review

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,9 +1,10 @@
 package ru.netology.sqr;
+
 public class SQRService {
-    public int sqrtCount(int minRange, int maxRange){
-        int count=0;
-        for (int i = 0; i*i <= maxRange; i++) {
-            if (minRange<=i*i && i*i<=maxRange){
+    public int sqrtCount(int minRange, int maxRange) {
+        int count = 0;
+        for (int i = 0; i * i <= maxRange; i++) {
+            if (minRange <= i * i && i * i <= maxRange) {
                 count++;
             }
         }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class SQRServiceTest {
     @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/valuesRange.csv")
-    public void testFieldSqrt(int expected, int minRange, int maxRange){
-        SQRService service=new SQRService();
-        int actual=service.sqrtCount(minRange,maxRange);
-        Assertions.assertEquals(expected,actual);
+    @CsvFileSource(files = "src/test/resources/valuesRange.csv")
+    public void testFieldSqrt(int expected, int minRange, int maxRange) {
+        SQRService service = new SQRService();
+        int actual = service.sqrtCount(minRange, maxRange);
+        Assertions.assertEquals(expected, actual);
     }
 }
