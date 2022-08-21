18ef40abc4c54018be7e8fe053398494ddd4d172
Daemontrit <slavikk1999@yandex.ru>
2022-08-21 14:06:45 +0300

    tests are now running

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class SQRServiceTest {
+
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/valuesRange.csv")
     public void testFieldSqrt(int expected, int minRange, int maxRange) {
         SQRService service = new SQRService();
         int actual = service.sqrtCount(minRange, maxRange);
+
         Assertions.assertEquals(expected, actual);
     }
 }
